f1ff8370c01413dbcfcdcfcfd9b5d9b6e19885b9
Erik Behrends <behrends@users.noreply.github.com>
2024-09-11 16:21:19 +0200

    Dateien in .idea-Ordner

--- app/src/main/java/com/example/todosandroid/MainActivity.kt
+++ app/src/main/java/com/example/todosandroid/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TodosAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Androids",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
