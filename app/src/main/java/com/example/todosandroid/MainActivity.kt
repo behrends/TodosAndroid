bf7ec25a7345d3c161b492e2e2c11799beec31b2
Erik Behrends <behrends@users.noreply.github.com>
2024-09-12 12:29:38 +0200

    Todo mit Checkbox in Row

--- app/src/main/java/com/example/todosandroid/MainActivity.kt
+++ app/src/main/java/com/example/todosandroid/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.todosandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,13 +19,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Todo(text: String) {
-    Text(text)
+fun Todo(name: String) {
+    Row {
+        Checkbox(checked = false, onCheckedChange = { })
+        Text(name)
+    }
 }
 
-@Preview(showBackground = true,
+@Preview(
+    showBackground = true,
     showSystemUi = true,
-    name = "Todo Vorschau")
+    name = "Todo Vorschau"
+)
 @Composable
 fun TodoPreview() {
     Todo("Sport machen")
