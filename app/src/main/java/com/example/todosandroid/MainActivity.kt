1f3c60f5b8b19829a241cd4f3b5ad49d3dafe68b
Erik Behrends <behrends@users.noreply.github.com>
2024-09-12 10:55:25 +0200

    TodoPreview Composable zur Vorschau

--- app/src/main/java/com/example/todosandroid/MainActivity.kt
+++ app/src/main/java/com/example/todosandroid/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,4 +19,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Todo(text: String) {
     Text(text)
+}
+
+@Preview
+@Composable
+fun TodoPreview() {
+    Todo("Sport machen")
 }
