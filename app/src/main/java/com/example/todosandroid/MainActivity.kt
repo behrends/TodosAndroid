0a972b1d9904f23f8daa55407bec3ef2f370ea6e
Erik Behrends <behrends@users.noreply.github.com>
2024-09-16 10:24:41 +0200

    Layout mit Scaffold

--- app/src/main/java/com/example/todosandroid/MainActivity.kt
+++ app/src/main/java/com/example/todosandroid/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.todosandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,15 +23,18 @@ import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            TodoList()
+            Scaffold() { innerPadding ->
+                TodoList(modifier = Modifier.padding(innerPadding))
+            }
         }
     }
 }
 
 @Composable
-fun TodoList() {
-    Column {
+fun TodoList(modifier: Modifier = Modifier) {
+    Column(modifier) {
         Todo("Einkaufen")
         Todo("Android lernen")
         Todo("Sport machen")
