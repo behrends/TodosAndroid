bf59f3700d8116bc1a1b870a38a8788df2d083cb
Erik Behrends <behrends@users.noreply.github.com>
2024-09-18 18:07:41 +0200

    BottomAppBar eingeführt

--- app/src/main/java/com/example/todosandroid/MainActivity.kt
+++ app/src/main/java/com/example/todosandroid/MainActivity.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.room.Room
 import com.example.todosandroid.data.AppDatabase
 import com.example.todosandroid.data.TodoDao
@@ -27,7 +34,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodosAndroidTheme {
-                HomeScreen(todoDao = todoDao)
+                Scaffold(bottomBar = {
+                    BottomAppBar(actions = {
+                        IconButton(onClick = {}) {
+                            Icon(Icons.Filled.Home, contentDescription = "Todos")
+                        }
+                        IconButton(onClick = {}) {
+                            Icon(
+                                Icons.Filled.Settings,
+                                contentDescription = "Einstellungen",
+                            )
+                        }
+                    })
+                }) { innerPadding ->
+                    HomeScreen(todoDao = todoDao)
+                }
             }
         }
     }
