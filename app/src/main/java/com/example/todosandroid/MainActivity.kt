b234d79cde16c0ed41d3d89f6dbc6f7828e28ccd
Erik Behrends <behrends@users.noreply.github.com>
2024-09-18 18:15:59 +0200

    NavHost deklariert die Ziele / Screens der Navigation

--- app/src/main/java/com/example/todosandroid/MainActivity.kt
+++ app/src/main/java/com/example/todosandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
@@ -11,11 +12,15 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import com.example.todosandroid.data.AppDatabase
 import com.example.todosandroid.data.TodoDao
 import com.example.todosandroid.ui.HomeScreen
+import com.example.todosandroid.ui.SettingsScreen
 import com.example.todosandroid.ui.theme.TodosAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,7 +54,18 @@ class MainActivity : ComponentActivity() {
                         }
                     })
                 }) { innerPadding ->
-                    HomeScreen(todoDao = todoDao)
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("home") {
+                            HomeScreen(todoDao)
+                        }
+                        composable("settings") {
+                            SettingsScreen()
+                        }
+                    }
                 }
             }
         }
