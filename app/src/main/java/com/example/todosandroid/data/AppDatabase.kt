dc22172ef9a71730c4fe65f9980e4725098c0dfd
Erik Behrends <behrends@users.noreply.github.com>
2024-10-21 12:27:23 +0200

    Singleton der DB mit "also"-Funktion erzeugt

--- app/src/main/java/com/example/todosandroid/data/AppDatabase.kt
+++ app/src/main/java/com/example/todosandroid/data/AppDatabase.kt
@@ -16,14 +16,10 @@ abstract class AppDatabase : RoomDatabase() {
 
         fun getInstance(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "todo-database"
-                ).build()
-                INSTANCE = instance
-                instance
+                Room.databaseBuilder(context, AppDatabase::class.java, "todo-database")
+                    .build().also { INSTANCE = it }
             }
+
         }
     }
 }
